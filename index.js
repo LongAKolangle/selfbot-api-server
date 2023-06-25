f49890e0dc5586b737167f4fa005f927392af49d
LongAKolangle <thanhlong0975855842@gmail.com>
2023-06-25 13:44:39 +0700

    second commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import * as express from "express"
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Our API Server!\nCreated by Eternityyy from BKI Team")
+})
+
+app.get("/api/quote", (req, res) => {
+    const data = {"quote": "hello world"}
+    res.json(data)
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
