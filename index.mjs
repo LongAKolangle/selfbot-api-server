dccd2ee33bc9b7d15303714e92d52461230ce846
LongAKolangle <thanhlong0975855842@gmail.com>
2023-06-25 19:30:37 +0700

    Final Quote API

--- index.mjs
+++ index.mjs
@@ -1,16 +1,12 @@
 const messageElement = document.getElementById('content');
 
-
 function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
-// Fetch a random quote from the API and update the HTML
 
-window.addEventListener('DOMContentLoaded', async () => {
-    fetch('./data/quotes.json')
+fetch('./data/quotes.json')
         .then(response => response.json())
         .then(data => {
             const index = randomInt(0, data.length)
             messageElement.textContent = data[index]
         })
-});
