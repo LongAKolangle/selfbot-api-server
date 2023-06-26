454c1f4ff1704f994717afce96230d35d6b5aa80
LongAKolangle <thanhlong0975855842@gmail.com>
2023-06-26 22:14:24 +0700

    23

--- api/index.js
+++ api/index.js
@@ -9,12 +9,4 @@ router.get('/', (req, res) => {
     res.json("Hello World!");
 });
 
-router.get('/quote', (req, res) => {
-    const data = JSON.parse(fs.readFileSync(path.resolve("./data", "quotes.json"), 'utf-8'));
-    const index = Math.floor(Math.random() * data.length);
-    res.setHeader('Content-Type', 'application/json');
-    res.statusCode = 200;
-    res.json(data[index]);
-});
-
 export default router;
