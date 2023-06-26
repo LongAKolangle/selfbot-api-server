ec96048d03aa76173a2d9b8c8c4f0ff59ad25fd3
LongAKolangle <thanhlong0975855842@gmail.com>
2023-06-26 22:26:34 +0700

    26

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 import express from "express"
+import fs from "fs"
+import path from "path"
 const router = express.Router();
 router.get('/api', (req, res) => {
     res.statusCode = 200;
@@ -10,5 +12,14 @@ router.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
 });
+
+router.get('/api/quote', (req, res) => {
+    const data = JSON.parse(fs.readFileSync(path.resolve("./data", "quotes.json"), 'utf-8'));
+    const index = Math.floor(Math.random() * data.length);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data[index]);
+});
+
 
 export default router;
