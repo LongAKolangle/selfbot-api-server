19758a6b42ad5999ad2e98116c2ca8f905fbb58e
LongAKolangle <thanhlong0975855842@gmail.com>
2023-06-26 21:50:27 +0700

    18

--- api/quote.js
+++ api/quote.js
@@ -1,17 +1,13 @@
-import path from "path";
 import express from "express"
 import fs from "fs"
 const router = express.Router();
 
-function getRandomQuote() {
-    const quotesPath = path.join(__dirname, 'data/quotes.json');
-    const data = JSON.parse(fs.readFileSync(quotesPath, 'utf-8'));
-    const index = Math.floor(Math.random() * data.length);
-    return data[index];
-}
-
 router.get('/api/quote', (req, res) => {
-  res.json(getRandomQuote());
+    const data = JSON.parse(fs.readFileSync("../data/quotes.json", 'utf-8'));
+    const index = Math.floor(Math.random() * data.length);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(data[index]);
 });
 
 export default router;
